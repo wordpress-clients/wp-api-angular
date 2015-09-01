2df4b1317eeb45f88d826bafb619f8955642c241
shprink <julien.renaux@gmail.com>
2015-09-01 01:22:04 -0500

    mapping all Posts

--- dist/wp-api-angularjs.js
+++ dist/wp-api-angularjs.js
@@ -156,12 +156,12 @@
 	'use strict';
 	
 	Object.defineProperty(exports, '__esModule', {
-	  value: true
+	    value: true
 	});
 	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
-	var _get = function get(_x4, _x5, _x6) { var _again = true; _function: while (_again) { var object = _x4, property = _x5, receiver = _x6; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x4 = parent; _x5 = property; _x6 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 	
@@ -174,47 +174,88 @@
 	var _parentServiceJs2 = _interopRequireDefault(_parentServiceJs);
 	
 	var _default = (function (_Parent) {
-	  _inherits(_default, _Parent);
+	    _inherits(_default, _Parent);
 	
-	  function _default() {
-	    _classCallCheck(this, _default);
+	    function _default() {
+	        _classCallCheck(this, _default);
 	
-	    _get(Object.getPrototypeOf(_default.prototype), 'constructor', this).apply(this, arguments);
-	  }
-	
-	  _createClass(_default, [{
-	    key: 'getList',
-	
-	    /**
-	     * @ngdoc function
-	     * @name wp-api-angularjs.$wpApiPosts#getList
-	     * @access public
-	     * @methodOf wp-api-angularjs.$wpApiPosts
-	     *
-	     * @description
-	     * Get post list
-	     * @param {object} params  Optional: {Object.<string|Object>} – Map of strings or objects which will be serialized with the paramSerializer and appended as GET parameters.
-	     * @param {object} data  Optional: {string|Object} – Data to be sent as the request message data.
-	     * @param {object} headers  Optional: {Object} – Map of strings or functions which return strings representing HTTP headers to send to the server. If the return value of a function is null, the header will not be sent. Functions accept a config object as an argument.
-	     * @example
-	     * $wpApiPosts.getList({
-	     *  page: 1,
-	     *  per_page: 10,
-	     *  "filter[orderby]": "date"
-	     *  "filter[orderby]": "asc"
-	     * });
-	     * @returns {Object} Promise
-	     */
-	    value: function getList() {
-	      var params = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-	      var data = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
-	      var headers = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
-	
-	      return _get(Object.getPrototypeOf(_default.prototype), 'getList', this).call(this, '/posts', params, data, headers);
+	        _get(Object.getPrototypeOf(_default.prototype), 'constructor', this).apply(this, arguments);
 	    }
-	  }]);
+	
+	    _createClass(_default, [{
+	        key: 'getList',
+	
+	        /**
+	         * @ngdoc function
+	         * @name wp-api-angularjs.$wpApiPosts#getList
+	         * @access public
+	         * @methodOf wp-api-angularjs.$wpApiPosts
+	         *
+	         * @description
+	         * Get post list
+	         * @param {object} params  Optional: {Object.<string|Object>} – Map of strings or objects which will be serialized with the paramSerializer and appended as GET parameters.
+	         * @param {object} data  Optional: {string|Object} – Data to be sent as the request message data.
+	         * @param {object} headers  Optional: {Object} – Map of strings or functions which return strings representing HTTP headers to send to the server. If the return value of a function is null, the header will not be sent. Functions accept a config object as an argument.
+	         * @example
+	         * $wpApiPosts.getList({
+	         *  page: 1,
+	         *  per_page: 10,
+	         *  "filter[orderby]": "date"
+	         *  "filter[orderby]": "asc"
+	         * });
+	         * @returns {Object} Promise
+	         */
+	        value: function getList(params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'getList', this).call(this, '/posts', params, data, headers);
+	        }
+	    }, {
+	        key: 'get',
+	        value: function get(postId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'get', this).call(this, '/posts/' + postId, params, data, headers);
+	        }
+	    }, {
+	        key: 'getMetaList',
+	        value: function getMetaList(postId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'getList', this).call(this, '/posts/' + postId + '/meta', params, data, headers);
+	        }
+	    }, {
+	        key: 'getMeta',
+	        value: function getMeta(postId, metaId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'get', this).call(this, '/posts/' + postId + '/meta/' + metaId, params, data, headers);
+	        }
+	    }, {
+	        key: 'getRevisionList',
+	        value: function getRevisionList(postId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'getList', this).call(this, '/posts/' + postId + '/revisions', params, data, headers);
+	        }
+	    }, {
+	        key: 'getRevision',
+	        value: function getRevision(postId, revisionId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'get', this).call(this, '/posts/' + postId + '/revisions/' + revisionId, params, data, headers);
+	        }
+	    }, {
+	        key: 'getCategoryList',
+	        value: function getCategoryList(postId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'getList', this).call(this, '/posts/' + postId + '/terms/category', params, data, headers);
+	        }
+	    }, {
+	        key: 'getCategory',
+	        value: function getCategory(postId, categoryId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'get', this).call(this, '/posts/' + postId + '/terms/category/' + categoryId, params, data, headers);
+	        }
+	    }, {
+	        key: 'getTagList',
+	        value: function getTagList(postId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'getList', this).call(this, '/posts/' + postId + '/terms/tag', params, data, headers);
+	        }
+	    }, {
+	        key: 'getTag',
+	        value: function getTag(postId, tagId, params, data, headers) {
+	            return _get(Object.getPrototypeOf(_default.prototype), 'get', this).call(this, '/posts/' + postId + '/terms/tag/' + tagId, params, data, headers);
+	        }
+	    }]);
 
-	  return _default;
+	    return _default;
 	})(_parentServiceJs2['default']);
 
 	exports['default'] = _default;
@@ -251,6 +292,19 @@
 	    _createClass(_default, [{
 	        key: 'getList',
 	        value: function getList(suffix) {
+	            var params = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+	            var data = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+	            var headers = arguments.length <= 3 || arguments[3] === undefined ? {} : arguments[3];
+	
+	            return this.$http.get(this.baseUrl + suffix, {
+	                params: params,
+	                data: data,
+	                headers: headers
+	            });
+	        }
+	    }, {
+	        key: 'get',
+	        value: function get(suffix) {
 	            var params = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 	            var data = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
 	            var headers = arguments.length <= 3 || arguments[3] === undefined ? {} : arguments[3];
--- test/controller.js
+++ test/controller.js
@@ -9,14 +9,42 @@ export default function($scope, $wpApiPosts, $wpApiPages) {
      * POSTS
      */
 
-    $wpApiPosts.getList({
-            page: 2,
-            per_page: 2,
+    let postsMethods = {
+        getList: [{
+            page: 1,
+            per_page: 1,
             "filter[orderby]": "date",
             "filter[order]": "desc",
-        })
-        .then(() => vm.posts.getList = 'OK')
-        .catch(() => vm.posts.getList = 'KO');
+        }],
+        get: [2016],
+        getMetaList: [2016],
+        getMeta: [2016],
+        getRevisionList: [2016],
+        getRevision: [2016],
+        getCategoryList: [2016],
+        getCategory: [2016],
+        getTagList: [2016],
+        getTag: [2016]
+    }
+
+    $wpApiPosts.getList({
+        page: 1,
+        per_page: 1,
+        "filter[orderby]": "date",
+        "filter[order]": "desc",
+    }).then(() => {
+
+    })
+    angular.forEach(postsMethods, (parameters, name) => {
+        $wpApiPosts[name].apply($wpApiPosts, parameters)
+            .then(() => vm.posts[name] = 'OK')
+            .catch(() => vm.posts[name] = 'KO');
+    });
+
+
+
+
+
 
     /*
      * PAGES
